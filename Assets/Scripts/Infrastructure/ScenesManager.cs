85f83c05473de38d0bf2acb47d0c52518e15660b
educationevolution <159627295+educationevolution@users.noreply.github.com>
2024-05-30 22:45:50 +0300

    Create Main and Polls scenes

---
+++ Assets/Scripts/Infrastructure/ScenesManager.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Scripts.Infrastructure
+{
+    public enum SceneName
+    {
+        None,
+        Main,
+        Poll
+    }
+
+    public class ScenesManager : MonoBehaviour
+    {
+        [SerializeField] private int _defaultTargetFrameRate = 60;
+        public static ScenesManager Instance { get; private set; }
+        private SceneName? _currentScene;
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = _defaultTargetFrameRate;
+            Instance = this;
+            transform.SetParent(null);
+            DontDestroyOnLoad(gameObject);
+            var currentSceneName = SceneManager.GetActiveScene().name;
+            Enum.TryParse(currentSceneName, out SceneName sceneName);
+            _currentScene = sceneName;
+        }
+
+        public void LoadScene(SceneName sceneToLoad)
+        {
+            if (_currentScene == sceneToLoad)
+            {
+                throw new Exception($"Scene {sceneToLoad} is already opened!");
+            }
+            _currentScene = sceneToLoad;
+            SceneManager.LoadScene(_currentScene.ToString());
+        }
+    }
+}
---
+++ Assets/Scripts/Screens/Main/MainScreenManager.cs
@@ -0,0 +1,24 @@
+using Scripts.Infrastructure;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts.Screens.Main
+{
+    public class MainScreenManager : MonoBehaviour
+    {
+        [SerializeField] private Button _startPollButton;
+
+        private void Awake()
+        {
+            _startPollButton.onClick.AddListener(StartPollClickedCallback);
+        }
+
+        private void StartPollClickedCallback()
+        {
+            ScenesManager.Instance.LoadScene(SceneName.Poll);
+        }
+    }
+}
