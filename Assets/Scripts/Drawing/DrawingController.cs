99f2689411ad23bc218226c0ffe0284fdfdab4f8
educationevolution <159627295+educationevolution@users.noreply.github.com>
2024-05-31 10:25:31 +0300

    Created a basic drawing tool for drawing over poll options

---
+++ Assets/Scripts/Drawing/DrawingController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Infrastructure
+{
+    public class DrawingController : MonoBehaviour
+    {
+        [SerializeField] private Camera _camera;
+        [SerializeField] private DrawingLine _linePrefab;
+        [SerializeField] private Transform _linesContainer;
+        private Dictionary<int, DrawingLine> _drawingLinesById = new();
+        private int _currentLineId = 0;
+
+        private bool _isButtonDown = false;
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                _isButtonDown = true;
+                var newLine = Instantiate(_linePrefab, _linesContainer);
+                newLine.transform.position = GetMouseWorldPosition;
+                _currentLineId += 1;
+                _drawingLinesById[_currentLineId] = newLine;
+                return;
+            } 
+            if (Input.GetMouseButtonUp(0))
+            {
+                _isButtonDown = false;
+            }
+            if (_isButtonDown)
+            {
+                _drawingLinesById[_currentLineId].AddPointIfPossible(GetMouseWorldPosition);
+            }
+        }
+
+        private Vector3 GetMouseWorldPosition
+        {
+            get 
+            {
+                var mousePosition = Input.mousePosition;
+                mousePosition.z += _camera.transform.position.z * -1;
+                return _camera.ScreenToWorldPoint(mousePosition);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Drawing/DrawingLine.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Infrastructure
+{
+    public class DrawingLine : MonoBehaviour
+    {
+        [SerializeField] private LineRenderer _lineRenderer;
+        [SerializeField] private float _minDistanceBetweenPoints = 0.1f;
+
+        private void Start()
+        {
+            _lineRenderer.SetPosition(0, transform.position);
+            _lineRenderer.positionCount = 1;
+        }
+
+        public void AddPointIfPossible(Vector3 worldPosition)
+        {
+            worldPosition.z = transform.parent.position.z;
+            var lastPosition = _lineRenderer.GetPosition(_lineRenderer.positionCount - 1);
+            var distanceFromLastPosition = Vector3.Distance(lastPosition, worldPosition);
+            if (distanceFromLastPosition < _minDistanceBetweenPoints)
+            {
+                return;
+            }
+            _lineRenderer.positionCount += 1;
+            _lineRenderer.SetPosition(_lineRenderer.positionCount - 1, worldPosition);
+        }
+    }
+}
